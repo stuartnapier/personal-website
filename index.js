2521f22cf63d8a5b356b2ad09bb71d371534e315
stuartnapier <stuartjnapier@gmail.com>
2024-07-29 22:21:36 +0100

    added small amount of javascript. removed placeholder text from project cards

---
+++ index.js
@@ -0,0 +1,8 @@
+// animate the hero link arrow
+$(".hero-link").on("mouseover", function(event){
+    $(".hero-link svg").animate({paddingLeft: "10px"});
+})
+
+$(".hero-link").on("mouseleave", function(event){
+    $(".hero-link svg").animate({paddingLeft: "0px"});
+})
