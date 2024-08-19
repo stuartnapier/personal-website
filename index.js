a33d9ae93f1ae61d01ce9fa3b09dfc162d9f8ad4
stuartnapier <stuartjnapier@gmail.com>
2024-08-19 16:58:05 +0100

    refreshed the project section built with grid instead of flexbox now. adding basid javascript for dropdown menu

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ $(".hero-link").on("mouseover", function(event){
 $(".hero-link").on("mouseleave", function(event){
     $(".hero-link svg").animate({paddingLeft: "0px"});
 })
+
+
+
+
+   	
+
+$( ".burger-button" ).on( "click", function() {
+    $( ".burger-dropdown" ).toggle();
+  });
