2438ae8a96ebd9d0d44e365a810d33fe51ee4634
stuartnapier <stuartjnapier@gmail.com>
2024-08-26 21:16:01 +0100

    added button popover for contact me

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 // animate the hero link arrow
+// $(".hero-link").on("mouseover", function(event){
+//     $(".hero-link svg").animate({paddingLeft: "10px"});
+// })
+
 $(".hero-link").on("mouseover", function(event){
-    $(".hero-link svg").animate({paddingLeft: "10px"});
+    $(".hero-link svg").animate({left: "10px"});
 })
 
 $(".hero-link").on("mouseleave", function(event){
-    $(".hero-link svg").animate({paddingLeft: "0px"});
+    $(".hero-link svg").animate({left: "0px"});
 })
 
 
@@ -15,3 +19,10 @@ $(".hero-link").on("mouseleave", function(event){
 $( ".burger-button" ).on( "click", function() {
     $( ".burger-dropdown" ).toggle();
   });
+
+// without this the drop down would still be present when the window size was increased then decreased back
+$(window).on('resize', function(){
+    if ($(window).width() >= 992){
+        $( ".burger-dropdown" ).hide();
+    } 
+});
